9b4dfcbc668c83d857d1ba731d870298209250bc
aleksander przeszlo <aprzeszlo@box.com>
2025-03-22 13:25:02 +0100

    Mobile: kurs w liczbach

--- components/KursWLiczbach.tsx
+++ components/KursWLiczbach.tsx
@@ -5,14 +5,14 @@ export type KursWLiczbachItem = [string, React.ReactNode];
 
 const KursWLiczbach = ({ items }: { items: KursWLiczbachItem[] }) => {
   return (
-    <HomeSection className="flex flex-col justify-start items-start gap-8 bg-eblue py-28 px-8 md:px-6 md:px-24 md:py-12 font-outfit text-butter-100">
-      <h1 className="md:hidden font-medium font-monarcha text-4xl text-electric-500 pb-4">
+    <HomeSection className="flex flex-col justify-start md:justify-between items-start md:gap-4 gap-0 bg-eblue py-28 px-8 md:px-6 md:px-24 md:py-12 font-outfit text-butter-100">
+      <h1 className="md:hidden font-medium font-monarcha text-4xl text-electric-500 pb-12 md:pb-4 ">
         Kurs w <br /> liczbach
       </h1>
-      <section>
+      <>
         {items.map(([liczba, opis]) => (
           <div
-            className="flex justify-between items-center gap-8 border-ewhite border-b-[1px]"
+            className="flex justify-between w-full items-center gap-8 md:pb-8 border-ewhite border-b-[1px]"
             key={liczba}
           >
             <span className="text-5xl md:text-7xl">{liczba}</span>
@@ -21,7 +21,7 @@ const KursWLiczbach = ({ items }: { items: KursWLiczbachItem[] }) => {
             </span>
           </div>
         ))}
-      </section>
+      </>
     </HomeSection>
   );
 };
